d8a94d2c4ae85bc01acbb48e1ba47479cc5f9c62
Code Expert <s22cpjeel107@gmail.com>
2024-08-04 17:15:11 +0530

    oops

---
+++ OOPS/class and objects.py
@@ -0,0 +1,18 @@
+
+class Students:
+    name= "Jeel Patel"
+    roll_no= 24
+    sub= "B.tech CSE"
+
+stu= Students()
+
+print(stu.name)
+print(stu.roll_no)
+print(stu.sub)
+
+print("\n")
+stu2= Students()
+
+print(stu2.name)
+print(stu2.roll_no)
+print(stu2.sub)
