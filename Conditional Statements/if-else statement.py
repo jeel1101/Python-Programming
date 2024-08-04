267a7d0d22c6c8a5b7d3c7bfd0fc5bff1197eb5c
Code Expert <s22cpjeel107@gmail.com>
2024-08-04 17:08:58 +0530

    Conditional Statements

---
+++ Conditional Statements/if-else statement.py
@@ -0,0 +1,7 @@
+age= int(input("Enter your age: "))
+
+if(age>=18):
+    print(f"\nYour age is {age}, you can drive")
+
+else:
+    print(f"\nYour age is {age}, you cannot drive")
---
+++ Conditional Statements/if-else-elif statement.py
@@ -0,0 +1,13 @@
+color= input("Enter the color: ")
+
+if(color=="red" or color== "Red"):
+    print("\nStop at signal")
+
+elif(color=="orange" or color=="Orange"):
+    print("\nBe ready and start your vehicle")
+
+elif(color=="green" or color=="Green"):
+    print("\nGo")
+    
+else:
+    print("\nSomething went wrong")
